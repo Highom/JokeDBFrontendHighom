d912a475f9b6408e54f1494e387615071912fe6c
Highom <highomlp@gmail.com>
2020-09-21 17:39:59 +0200

    added dialog

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import {TableCell, TableBody,Table, Typography, Button, TableContainer, TableHead, TableRow} from '@material-ui/core';
+import {TableCell, TableBody,Table, Button, TableContainer, TableHead, TableRow, Dialog, DialogTitle, DialogContent, TextField, Select} from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Paper from '@material-ui/core/Paper';
 const axios = require('axios');
 
 function App() {
   const [jokes,setJokes] = useState([]);
- let api = "http://localhost:8080/sql/jokes";
+  const [request, setRequest] = useState([]);
+  const [database, setDatabase] = React.useState("sql")
+  const [open, setOpen] = React.useState(false);
+  let api = `http://localhost:8080/${database}/jokes`;
 
  
   useEffect( () => {
@@ -26,11 +29,97 @@ function App() {
       }
   }
 
+  const onEdit  = joke => {
+    setRequest({
+      path: `${api}/${joke.id}`,
+      type: "PUT",
+      text: joke.text,
+      rating: joke.rating
+    });
+    setOpen(true);
+  }
+
+  const onAdd  = () => {
+    setRequest({
+      path: `${api}`,
+      type: "POST",
+      text: "",
+      rating: null
+    });
+    setOpen(true);
+  }
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChange = (event) => {
+    setDatabase(event.target.value)
+  };
+
+ const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+    axios({
+      method: request.type,
+      url: request.path,
+      data: event.target.value
+    }).catch( err => {console.log(err)});
+    handleClose();
+ }
+
+
   return (
     <div className="App">
-      <header>
-        <Typography>test</Typography>
-      </header>
+      <Select
+        native
+        value={database}
+        onChange={handleChange}
+        inputProps={{
+          name: 'Database',
+          id: 'DatabaseSelect',
+        }}
+      >
+        <option value="sql">SQL</option>
+        <option value="mdb">MDB</option>
+        <option value="csv">CSV</option>
+      </Select>
+      <Button
+        color="primary"
+        onClick={() => onAdd()}>
+        Add new Joke
+      </Button>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="newJokeDialog">
+        <DialogTitle id="newJokeDialog">Edit/Add Joke</DialogTitle>
+        <DialogContent>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="text"
+              label="Joke"
+              fullWidth
+              defaultValue={request.text}
+            />
+            <TextField
+              margin="dense"
+              id="rating"
+              label="Rating"
+              type="number"
+              fullWidth
+              defaultValue={request.rating}
+            />
+            <div>
+            <Button onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button color="primary" type="submit" label="submit">
+              Submit
+            </Button>
+            </div>
+          </form>
+        </DialogContent>
+      </Dialog>
      <TableContainer component={Paper}>
       <Table aria-label="simple table">
         <TableHead>
@@ -39,8 +128,6 @@ function App() {
             <TableCell>text</TableCell>
             <TableCell>rating</TableCell>
             <TableCell>date</TableCell>
-            <TableCell>Edit</TableCell>
-            <TableCell>Delete</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -52,13 +139,12 @@ function App() {
               <TableCell>{joke.text}</TableCell>
               <TableCell>{joke.rating}</TableCell>
               <TableCell>{joke.date}</TableCell>
-              <TableCell>
+              <TableCell align="right">
                 <Button variant="contained"
-                  color="primary">
+                  color="primary"
+                  onClick={() => onEdit(joke)}>
                   Edit
                 </Button>
-              </TableCell>
-              <TableCell>
                 <Button variant="contained"
                   color="secondary"
                   onClick={() => onDelete(joke.id)}
